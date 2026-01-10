9619cae0b02cc2ed27c238125417e71b8c8f3484
Bukari Máté <matebukari@gmail.com>
2026-01-10 18:07:18 +0100

    chore: use environment variables for firebase config

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -4,12 +4,12 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Get these values from your Firebase Console -> Project Settings
 const firebaseConfig = {
-  apiKey: "AIzaSyBV-yh_vN1AqFAspir7w9RTMjCV85O0VzI",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: process.env.EXPO_Public_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_Public_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_Public_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
